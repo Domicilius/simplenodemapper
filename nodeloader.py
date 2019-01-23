8f6e760fd8dfafdf9456002443b4309bcf51522c
dom <mason.theodorej@gmail.com>
2019-01-23 14:40:16 -0800

    Add implementation for fixed-point nodes

--- nodeloader.py
+++ nodeloader.py
@@ -2,36 +2,41 @@ import yaml
 import networkx as nx
 import matplotlib.pyplot as plt
 
-G = nx.Graph()
 
 class Node(yaml.YAMLObject):
-  yaml_tag = u'!Node'
-  def __init__(self, name, position, edges):
-    self.name = name
-    self.position = position
-    self.edges = edges
+    yaml_tag = u'!Node'
 
-  def __repr__(self):
-    return "%s(name=%r, position=%r, edges=%r)" % (self.__class__.__name__, self.name, self.position, self.edges)
+    def __init__(self, name, position, edges):
+        self.name = name
+        self.position = position
+        self.edges = edges
 
-edgelist=[]
-pos = []
+    def __repr__(self):
+        return "%s(name=%r, position=%r, edges=%r)" % (
+            self.__class__.__name__, self.name, self.position, self.edges)
+
+
+G = nx.Graph()
+
+
+edgelist = []
+pos = {}
 with open("nodedata.yaml", 'r') as stream:
-  for data in yaml.load_all(stream):
-    if data.edges:
-      for i in data.edges:
-        edgelist.append((data.name, i))
-    newpos = list(data.position)
-    pos[data] = newpos
+    for data in yaml.load_all(stream):
+        if data.edges:
+            for i in data.edges:
+                edgelist.append((data.name, i))
+        newpos = list(data.position)
+        newpos[0] = newpos[0] * 1000
+        newpos[1] = newpos[1] * 1000
+        pos[data.name] = newpos
 
 G.add_edges_from(edgelist)
 
-#pos = nx.shell_layout(G)
-nx.draw_networkx_nodes(G, pos, node_size=2000, node_shape='s')
+# pos = nx.shell_layout(G)
+nx.draw_networkx_nodes(G, pos, node_size=200, node_shape='s')
 nx.draw_networkx_edges(G, pos)
-nx.draw_networkx_labels(G, pos, font_size=20, front_family='sans-serif')
-
-print(pos)
+# nx.draw_networkx_labels(G, pos, font_size=5, font_family='sans-serif')
 
 plt.axis('off')
 plt.show()
